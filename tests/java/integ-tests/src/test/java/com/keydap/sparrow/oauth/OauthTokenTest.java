a61e45689afd313234924d54e674b2f21166bd52
Kiran Ayyagari <kayyagari@keydap.com>
2017-02-13 16:18:27 +0530

    call flush() to send the reponse

--- tests/java/integ-tests/src/test/java/com/keydap/sparrow/oauth/OauthTokenTest.java
+++ tests/java/integ-tests/src/test/java/com/keydap/sparrow/oauth/OauthTokenTest.java
@@ -118,6 +118,7 @@ public class OauthTokenTest {
                 }
                 br.close();
                 response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().flush();
             }
         };
         
