d7874bad4de436488f958a1be613b1865354e8d5
Kiran Ayyagari <kayyagari@keydap.com>
2017-09-06 22:43:27 +0530

    o build executables for various platforms o ignore dist directory

--- build-release.sh
+++ build-release.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 # builds x64 binaries for Linux, OS X and Windows operating systems
 
+DIST_DIR="dist"
+
 copyVersionFile() {
     cp resources/version.txt net/version.go
 }
 
-genVersionInfo() {
-
-}
 
 buildLinux() {
-    #env GOOS=linux GOARCH=arm go build -v github.com/path/to/your/app
+    env GOOS=linux GOARCH=386 go build -v -o $DIST_DIR/sparrow-linux32
+    env GOOS=linux GOARCH=amd64 go build -v -o $DIST_DIR/sparrow-linux64 
 }
+
+buildOsX() {
+    env GOOS=darwin GOARCH=amd64 go build -v -o $DIST_DIR/sparrow-darwin64
+}
+
+buildWindows() {
+    env GOOS=windows GOARCH=386 go build -v -o $DIST_DIR/sparrow-win32
+    env GOOS=windows GOARCH=amd64 go build -v -o $DIST_DIR/sparrow-win64
+}
+
+buildLinux
+buildOsX
+buildWindows
