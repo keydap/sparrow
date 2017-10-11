6d4653999ca2438e72f2129e9ba181ef507d3024
Kiran Ayyagari <kayyagari@keydap.com>
2017-10-11 06:27:33 +0530

    fixed tests

--- tests/java/integ-tests/src/test/java/com/keydap/sparrow/ldap/LdapSearchTest.java
+++ tests/java/integ-tests/src/test/java/com/keydap/sparrow/ldap/LdapSearchTest.java
@@ -6,14 +6,18 @@
  */
 package com.keydap.sparrow.ldap;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.apache.directory.api.ldap.model.cursor.EntryCursor;
+import org.apache.directory.api.ldap.model.entry.Entry;
 import org.apache.directory.api.ldap.model.message.SearchScope;
 import org.apache.directory.ldap.client.api.LdapNetworkConnection;
+import org.apache.directory.ldap.client.api.NoVerificationTrustManager;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  *
@@ -25,12 +29,11 @@ public class LdapSearchTest {
     
     @BeforeClass
     public static void connect() throws Exception {
+        //System.setProperty("javax.net.debug", "ssl");
         con = new LdapNetworkConnection("localhost", 7092);
+        con.getConfig().setTrustManagers(new NoVerificationTrustManager());
         con.connect();
-    }
-    
-    @Test
-    public void testBind() throws Exception {
+        con.startTls();
         con.bind("uid=admin,dc=example,dc=com", "secret");
     }
     
@@ -38,21 +41,35 @@ public class LdapSearchTest {
     public void testSearchSubtree() throws Exception {
         // subtree and onelevel are treated as same
         EntryCursor cursor = con.search("dc=example,dc=com", "(uid=*)", SearchScope.SUBTREE, "*");
+        int count = 0;
         while(cursor.next()) {
             assertNotNull(cursor.get());
+            count++;
         }
         
         assertNotNull(cursor.getSearchResultDone());
-        
+        assertTrue(count > 0);
         cursor.close();
     }
     
     @Test
     public void testSearchObject() throws Exception {
-        EntryCursor cursor = con.search("uid=admin,dc=example,dc=com", "(uid=*)", SearchScope.OBJECT, "*");
+        String userDn = "uid=admin,ou=Users,dc=example,dc=com";
+        String groupDn = "cn=Administrator,ou=Groups,dc=example,dc=com";
+
+        Entry user = searchOne(userDn, "(uid=*)");
+        assertEquals(groupDn.toLowerCase(), user.get("member").getString().toLowerCase());
+        
+        Entry group = searchOne(groupDn, "(cn=*)");
+        assertEquals(userDn.toLowerCase(), group.get("uniqueMember").getString().toLowerCase());
+    }
+    
+    private Entry searchOne(String dn, String filter) throws Exception {
+        EntryCursor cursor = con.search(dn, filter, SearchScope.OBJECT, "*");
         int count = 0;
+        Entry e = null;
         while(cursor.next()) {
-            System.out.println(cursor.get());
+            e = cursor.get();
             count++;
         }
         
@@ -60,5 +77,7 @@ public class LdapSearchTest {
         cursor.close();
         
         assertEquals(1, count);
+        
+        return e;
     }
 }
