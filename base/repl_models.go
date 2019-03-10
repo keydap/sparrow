35464fa0103412b306ef76f146a9c14253b6d9be
kayyagari <kayyagari@apache.org>
2019-03-10 20:48:20 +0100

    replication setup

---
+++ base/repl_models.go
@@ -0,0 +1,9 @@
+// Copyright 2019 Keydap. All rights reserved.
+// Licensed under the Apache License, Version 2.0, see LICENSE.
+
+package base
+
+type ConfigEvent struct {
+	Csn  string
+	Data []byte
+}
---
+++ net/repl_handler.go
@@ -0,0 +1,8 @@
+// Copyright 2019 Keydap. All rights reserved.
+// Licensed under the Apache License, Version 2.0, see LICENSE.
+
+package net
+
+func HandleReplEvent() {
+
+}
